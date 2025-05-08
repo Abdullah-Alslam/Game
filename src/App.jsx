cf395da08109f51165698e49e32dc1668586851f
Abdullah <abdullahxyzabc67@gmail.com>
2025-05-08 14:23:05 +0300

    edite app

--- src/App.jsx
+++ src/App.jsx
@@ -21,20 +21,17 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/signin" element={<SignIn />} />
 
-        
-          
-          <Route index element={<HomePage />} />
-          {/* Games */}
-          <Route path="games" element={<Games />} />
-          <Route path="sport" element={<Sport />} />
-          <Route path="war" element={<War />} />
-          <Route path="car" element={<Car />} />
-          <Route path="/gamedetail/:gameId" element={<GameDetail />} />
-         <Route path="contact" element={<Contact />} />
+        <Route index element={<HomePage />} />
+        {/* Games */}
+        <Route path="games" element={<Games />} />
+        <Route path="sport" element={<Sport />} />
+        <Route path="war" element={<War />} />
+        <Route path="car" element={<Car />} />
+        <Route path="/gamedetail/:gameId" element={<GameDetail />} />
+        <Route path="contact" element={<Contact />} />
         <Route path="blog" element={<Blog />} />
         <Route element={<RequireAuth />}>
           <Route path="forums" element={<Forums />} />
- 
         </Route>
         <Route path="*" element={<ERR404 />} />
       </Routes>
